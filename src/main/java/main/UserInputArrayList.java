6c747e3a641f7f317ccaafd9979044e9b4a2b6b4
Varun-3131 <srivastavvarun48@gmail.com>
2024-02-10 18:09:35 +0530

    Created ArrayList From User Input..

--- src/main/java/main/UserInputArrayList.java
+++ src/main/java/main/UserInputArrayList.java
@@ -1,7 +1,20 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UserInputArrayList {
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> list= new ArrayList<>();
+        System.out.print("Enter the no. of Element:-");
+        int n = sc.nextInt();
+        System.out.println("Enter the elemnts:-");
+        for (int i = 0; i <= n; i++) {
+            i = sc.nextInt();
+            list.add(i);
+        }
+        System.out.println(list);
+        sc.close();
     }
 }
